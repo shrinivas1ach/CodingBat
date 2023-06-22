618c21642051ccb85ba4487e8fea69204ddc2d4c
G Shrinivas Acharya <55631919+shrinivas1ach@users.noreply.github.com>
2023-06-22 16:14:36 +0530

    Create square.java

---
+++ square.java
@@ -0,0 +1,5 @@
+// Given a list of integers, return a list where each integer is multiplied with itself. 
+public List<Integer> square(List<Integer> nums) {
+  nums.replaceAll(n -> n * n);
+  return nums;
+}
