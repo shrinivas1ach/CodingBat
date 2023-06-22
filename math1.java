1f3725e6852ecb321cbdaa8361cb075b400dcc24
G Shrinivas Acharya <55631919+shrinivas1ach@users.noreply.github.com>
2023-06-22 16:16:55 +0530

    Create math1.java

---
+++ math1.java
@@ -0,0 +1,5 @@
+// Given a list of integers, return a list where each integer is added to 1 and the result is multiplied by 10.
+public List<Integer> math1(List<Integer> nums) {
+  nums.replaceAll(n -> (n+1) * 10);
+  return nums;
+}
