f211789e540ca0f410ea86fd4e490b208ca5450e
G Shrinivas Acharya <55631919+shrinivas1ach@users.noreply.github.com>
2023-06-22 16:21:33 +0530

    Create noNeg.java

---
+++ noNeg.java
@@ -0,0 +1,5 @@
+// Given a list of integers, return a list of the integers, omitting any that are less than 0.
+public List<Integer> noNeg(List<Integer> nums) {
+  nums.removeIf(n -> n < 0);
+  return nums;
+}
