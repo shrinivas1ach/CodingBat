8d01ea3aa98af016fff2110f8e2e564e59c0b784
G Shrinivas Acharya <55631919+shrinivas1ach@users.noreply.github.com>
2023-06-22 16:25:04 +0530

    Create noLong.java

---
+++ noLong.java
@@ -0,0 +1,5 @@
+// Given a list of strings, return a list of the strings, omitting any string length 4 or more.
+public List<String> noLong(List<String> strings) {
+  strings.removeIf(s -> s.length()>3);
+  return strings;
+}
